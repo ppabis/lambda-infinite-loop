84e7650aaa526e184a0d41de55746245331ed98e
ppabis <piotr@pabis.eu>
2023-07-19 23:18:48 +0200

    Function trigger

--- lambda.tf
+++ lambda.tf
@@ -17,3 +17,9 @@ resource "aws_lambda_function" "recursive-function" {
     }
   }
 }
+
+resource "aws_lambda_event_source_mapping" "recursive-function" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name    = aws_lambda_function.recursive-function.function_name
+  batch_size       = 1
+}
