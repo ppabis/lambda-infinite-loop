01f257fcb570e24c334bbc05a69311f27bf8b84d
ppabis <piotr@pabis.eu>
2023-07-19 23:14:15 +0200

    Required providers and gitignore

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+
+    archive = {
+      source = "hashicorp/archive"
+      version = ">= 2.0"
+    }
+  }
+}
